025dd3c0628d1e2f1eb363d9093530a52e2d4eec
19345a0211-M <manikantmeesala@gmail.com>
2023-06-30 12:17:19 +0000

    sub

---
+++ JavaProjects/src/Inheritance/C.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class C extends D {
+	
+	public void year() {
+		System.out.println("It is 2023");
+	}
+
+	public static void main(String[] args) {
+		C c = new C();
+		c.year();
+		c.date();
+		c.nextYear();
+
+	}
+
+}
