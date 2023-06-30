7c6615f3cf1bad6a16a8800903095d0430cd12ce
19345a0211-M <manikantmeesala@gmail.com>
2023-06-30 12:16:17 +0000

    sub code

---
+++ JavaProjects/src/Inheritance/B.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class B extends D{
+	
+	public void month() {
+		System.out.println("It is the month of June");
+	}
+
+	public static void main(String[] args) {
+		B b = new B();
+		b.month();
+		b.date();
+		b.nextYear();
+
+	}
+
+}
