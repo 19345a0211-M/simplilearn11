79d8ce62308b96031c6ff3be2c379019cb1962ea
19345a0211-M <manikantmeesala@gmail.com>
2023-07-04 11:45:40 +0000

    source code

---
+++ JavaProjects/src/jdbcDemo/Demo2.java
@@ -0,0 +1,38 @@
+package jdbcDemo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Demo2 {
+	public static void main(String[] args) throws ClassNotFoundException,SQLException {
+		String dburl = "jdbc:mysql://localhost:3306/Animated_Movies";//Connection String
+		String username = "root";
+		String password = "root";
+		String query = "Call SelectAllMovies();";
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con= DriverManager.getConnection (dburl, username, password); //Connecting to DB
+			Statement stmt = con.createStatement();//Execute the query
+			ResultSet rs = stmt.executeQuery(query);//Save the result after executing the query
+			
+			while(rs.next()) {
+				System.out.print("Title: " + rs.getString("title") + "\t");
+			    System.out.print("Genre: " + rs.getString("genre") + "\t");
+				System.out.print("Director: " + rs.getString("director") + "\t"); 
+				System.out.println("Release Year: "+ rs.getString("relese_year"));
+			}
+		}
+		
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		finally {
+			con.close();
+		}
+	}	
+}
